7e7a50cc578b01485c3f1d954e887d38ab6990a5
marija <marijaorlic14@gmail.com>
2023-07-03 10:55:10 +0200

    Kraj

--- src/new.jsx
+++ src/new.jsx
@@ -1,61 +1,78 @@
 import React from 'react';
+import { useState } from 'react';
 
+  const New = ({ handleAdd }) => {
+    const [userData, setUserData] = useState({      
+      title: '',
+      description: '',
+      amount: 0,
+      image: '',
+    });
+  
+    const handleInput = (e) => {
+      const { name, value } = e.target;
+      setUserData((prevData) => ({ ...prevData, [name]: value }));
+    };
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      handleAdd(userData);
+    };
+  
 
-const New = ({handleAdd, handleInput, userData}) => {
-  return (
-    <div className="page-wrapper bg-gra-01 p-t-30 p-b-30">
-      <div className="cont-black">
-        <h2 className="title">Add new swimsuit</h2>
-
-        <form id="novaKartica">
-          <div className="form-group">
-            <label htmlFor="title" className="lbl">
-              Title
-            </label>
-            <input
-              type="text"
-              name="ntitle"
-              id="title"
-              className="input--style-3 inpt-2"
-              placeholder=""
-              required
-              onInput={handleInput}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="desc" className="lbl">
-              Description
-            </label>
-            <input
-              type="text"
-              name="desc"
-              className="input--style-3 inpt-2"
-              placeholder=""
-              required
-              onInput={handleInput}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="image" className="lbl">
-              Image
-            </label>
-            <input
-              type="text"
-              name="image"
-              className="input--style-3 inpt-2"
-              placeholder=""
-              required
-              onInput={handleInput}
-            />
-          </div>
-
-          <div className="p-t-20 form-group">
-            <a className="btn" value="Add" readOnly onClick={() => {handleAdd(userData) }}>Add</a>
-          </div>
-        </form>
+    return (
+      <div className="page-wrapper bg-gra-01 p-t-30 p-b-30">
+        <div className="cont-black">
+          <h2 className="title">Add new swimsuit</h2>
+          <form id="novaKartica" onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="title" className="lbl">
+                Title
+              </label>
+              <input
+                type="text"
+                name="title"
+                id="title"
+                className="input--style-3 inpt-2"
+                placeholder=""
+                required
+                onChange={handleInput}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="desc" className="lbl">
+                Description
+              </label>
+              <input
+                type="text"
+                name="description"
+                className="input--style-3 inpt-2"
+                placeholder=""
+                required
+                onChange={handleInput}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="image" className="lbl">
+                Image
+              </label>
+              <input
+                type="text"
+                name="image"
+                className="input--style-3 inpt-2"
+                placeholder=""
+                required
+                onChange={handleInput}
+              />
+            </div>
+            <button className="btn1" type="submit">
+              Add
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
-  );
-}              
+    );
+  };
+  
 
 export default New;
--- src/products.jsx
+++ src/products.jsx
@@ -1,25 +1,63 @@
 import React from "react";
 import OneProduct from "./oneProduct";
+import { useState } from "react";
 
 const Products = ({ products, onAdd, onRemove, lovedNum }) => {  
+  const [currentPage, setCurrentPage] = useState(1);
+  const productsPerPage = 3; 
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
+
+  function Pagination({ currentPage, productsPerPage, totalProducts, paginate }) {
+    const pageNumbers = [];
+  
+    for (let i = 1; i <= Math.ceil(totalProducts / productsPerPage); i++) {
+      pageNumbers.push(i);
+    }
+  
     return (
+      <ul className="pagination">
+        {pageNumbers.map((number) => (
+          <li key={number} className={number === currentPage ? 'active' : ''}>
+            <button onClick={() => paginate(number)}>{number}</button>
+          </li>
+        ))}
+      </ul>
+    );
+  } 
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+    
+  return (
       <div className="main-text">
       <h3>Vote for your favourite swimsuit.</h3>
       <h3>The one with the most votes will be added to our new collection.</h3>
       
       <div className="all-products">
         
-        {products.map((product) => (
+      {currentProducts.map((product) => (
         <OneProduct 
-            key={product.id}    
-            product={product}
-            onAdd={onAdd}
-            onRemove={onRemove}
-            lovedNum={lovedNum} />
+          key={product.id}    
+          product={product}
+          onAdd={onAdd}
+          onRemove={onRemove}
+          lovedNum={lovedNum}
+        />
       ))}
+    </div>
+
+    <Pagination
+      currentPage={currentPage}
+      productsPerPage={productsPerPage}
+      totalProducts={products.length}
+      paginate={paginate}
+    />
 
       </div>
-      </div> 
+      
     );
   };
   
